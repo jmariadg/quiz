d84df86e4bc7e8b3991ed5fc09496a8b66d61509
Jose Maria Dominguez <jmariadg@hotmail.com>
2015-06-15 16:28:18 +0200

    Versión 6.1: Se añaden la nueva opción de datos de autor.

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,4 @@
-var express = require('express');
+		var express = require('express');
 var router = express.Router();
 
 var quizController = require('../controllers/quiz_controller');
@@ -8,8 +8,16 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz' });
 });
 
+/* Get author */
+router.get('/author', function(req, res){
+	res.render('author', { nombreautor: 'José Mª Domínguez' });
+});
+
 /* GET question and answer page */
 router.get('/quizes/question', quizController.question);
 router.get('/quizes/answer', quizController.answer);
+
+
+
 
 module.exports = router;
